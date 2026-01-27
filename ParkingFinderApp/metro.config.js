546517ce4d2ba5dc2c713995987c8c1265ebba70
mboo <mboo@ucsc.edu>
2026-01-26 21:20:27 -0800

    fixing

--- ParkingFinderApp/metro.config.js
+++ ParkingFinderApp/metro.config.js
@@ -1,12 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-///** @type {import('expo/metro-config').MetroConfig} */
-//const config = getDefaultConfig(__dirname);
-//getDefaultConfig.resolver.assetExts.push('cjs');
-
-//module.exports = config;
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
 config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
 
